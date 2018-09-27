ec8b30490733c9cab4ef64da6db5f2aa835176c2
comantxe <agm@maltiverse.com>
2018-09-27 18:47:47 +0200

    Corrected base64 padding errors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='maltiverse',
     packages=['maltiverse'],
-    version='0.6.5',
+    version='0.6.6',
     description='API wrapper for Maltiverse',
     author='Antonio Gomez',
     author_email='agm@maltiverse.com',
