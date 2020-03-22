bbb33bc26b5251d577e663dab04549ba601c5e62
comantxe <agm@maltiverse.com>
2020-03-22 16:33:58 +0100

    release 1.0.0

--- setup.py
+++ setup.py
@@ -3,13 +3,23 @@ from distutils.core import setup
 setup(
     name='maltiverse',
     packages=['maltiverse'],
-    version='0.7.8',
-    description='API wrapper for Maltiverse',
+    version='1.0.0',
+    license='MIT',
+    description='Python API wrapper for Maltiverse',
     author='Antonio Gomez',
     author_email='agm@maltiverse.com',
     url='https://github.com/maltiverse/maltiverse-python',
-    download_url='https://github.com/maltiverse/maltiverse-python',
+    download_url='https://github.com/maltiverse/python-maltiverse/archive/1.0.0.tar.gz',
     keywords=['maltiverse', 'API', 'threat intelligence', 'IoC', 'blacklist', 'search engine'],
-    classifiers=[],
-    requires=['requests', 'PyJWT']
+    install_requires=['requests', 'PyJWT'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3', 
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
