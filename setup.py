26d142a4d31bc1902bd7b3110467032bdb49e92f
David Gil <dgil@wocu-monitoring.com>
2024-04-18 18:07:21 +0200

    release 1.2.1 version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="maltiverse",
     packages=["maltiverse"],
-    version="1.2.0",
+    version="1.2.1",
     license="MIT",
     description="Python API wrapper for Maltiverse",
     author="Antonio Gomez",
